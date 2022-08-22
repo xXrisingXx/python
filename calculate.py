c81f8a2e540bdf786f0b5d34ecfa17a1fa692077
xXrisingXx <71821381+xXrisingXx@users.noreply.github.com>
2022-08-21 20:04:09 -0400

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -22,10 +22,12 @@ class calculator(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    #This is a hybrid command it works as a slash command "/calculate (number)"" and a text command "";calculate (number).""
     @commands.hybrid_command(name='calculate', with_app_command=True, describe="Calculates how many coins it cost for potions.")
     async def calculate(self, ctx, num):
-        #potion description
+        #Calculate description for the ui.
         "Calculates how many coins it cost for potions."
+        #This checks if someone uses numbers only, if they dont then it's false and will send a reponse.
         if str(num).isnumeric() == True:
             selection = Select(
                 placeholder="Choose a potion to calculate.⬇️",
@@ -55,19 +57,21 @@ class calculator(commands.Cog):
                     await ctx.send("```\n("+str(val1)+") portals roughly cost ("+str(num)+")\n```")
 
             selection.callback = my_callback 
-            
         elif str(num).isnumeric() == False:
+            #This is the responses it will send
             quotes = [
-                "I can't calculate letters idiot.",
+                "I can't calculate letters you idiot.",
                 "Letters are not aloud. Try again with a number idiot.",
             ]
             response = random.choice(quotes)
             await ctx.send("```\n"+response+"```")
 
+    #This sends the source code link on my github.
     @commands.hybrid_command()
     async def source(self, ctx):
+        #This embeds the message so you can click the link. If not embeded you link will just send as a text and not a link.
         embed = Embed
-        embed.description = "Here is my source code to this command (your_link_goes_here)."
+        embed.description = "The source code to my calculate function is located at this link (https://github.com/xXrisingXx/python/blob/main/calculate.py)."
         await ctx.send(embed=embed)
 
 async def setup(bot):
